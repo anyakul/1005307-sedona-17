7001431dcc4739454f0bc1346a7636e156cc73e8
anyakul <anyakulikova111@gmail.com>
2019-08-12 15:23:11 +0300

    продолжаем вёрстку личного проекта

--- source/js/modal-comment.js
+++ source/js/modal-comment.js
@@ -2,8 +2,8 @@
 var link = document.querySelector(".comment__button");
 var popup_error = document.querySelector(".modal-comment--error");
 var popup_sent = document.querySelector(".modal-comment--sent");
-var close_error = popup_error.querySelector(".modal-comment__button"); 
-var close_sent = popup_sent.querySelector(".modal-comment__button"); 
+var close_error = popup_error.querySelector(".modal-comment__button");
+var close_sent = popup_sent.querySelector(".modal-comment__button");
 var form = document.querySelector("form");
 var user_name = form.querySelector("[name=user-name]");
 var surname = form.querySelector("[name=surname]");
@@ -13,21 +13,21 @@ link.addEventListener("click", function (evt) {
     evt.preventDefault();
     if (!user_name.value  || !surname.value  || !phone.value || !email.value ) {
       evt.preventDefault();
-	  popup_error.classList.add("modal-show");
-	  popup_error.offsetWidth = popup_error.offsetWidth;
+    popup_error.classList.add("modal-show");
+    popup_error.offsetWidth = popup_error.offsetWidth;
     popup = popup_error;
   } else {
       popup_sent.classList.add("modal-show");
-	    popup_sent.offsetWidth = popup_sent.offsetWidth;
+      popup_sent.offsetWidth = popup_sent.offsetWidth;
       popup = popup_sent;
     }
-  })  
+  })
 close_error.addEventListener("click", function (evt) {
-    evt.preventDefault();	
+    evt.preventDefault();
     popup_error.classList.remove("modal-show");
 });
 close_sent.addEventListener("click", function (evt) {
-    evt.preventDefault();	
+    evt.preventDefault();
     popup_sent.classList.remove("modal-show");
 });
 window.addEventListener("keydown", function (evt) {
@@ -37,4 +37,4 @@ window.addEventListener("keydown", function (evt) {
         popup.classList.remove("modal-show");
       }
     }
-}); 
+});
