173b994f5df453e67b24b24c01c631bcfcb12b95
anyakul <anyakulikova111@gmail.com>
2019-08-23 23:45:37 +0300

    Делаю автоматическое обновление css

--- gulpfile.js
+++ gulpfile.js
@@ -105,17 +105,12 @@ gulp.task("build", gulp.series(
   "html"
 ));
 
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
-});
-
 var gulp = require("gulp");
   gulp.task("server", function () {
     server.init({
     server: "build/"
   });
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/less/**/*.less", gulp.series("css", "css-min", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
